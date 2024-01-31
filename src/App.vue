ab268559bb3b250f0b19fdff4322aeda2f5a5ce6
Авдеева Анастасия Андреевна <AAAvdeeva@synergy.ru>
2024-01-31 13:56:12 +0300

    'v-note'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>|
+    <router-link style="text-decoration: none; color: #282846;" to="/">Home</router-link> |
+    <router-link  style="text-decoration: none; color: #282846;" to="/about">About</router-link>
   </nav>
-
   <router-view />
 </template>
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-wrapper">
+    <h1>В этом приложении:</h1>
+    <ul>
+      <li>Полная настройка шаблона на Vue CLI</li>
+      <li>Установка и настройка Vue-router для Vue.js 3</li>
+      <li>Установка и настройка Vuex</li>
+      <li>Работа с LocalStore</li>
+      <li>Использование препроцессора SCSS</li>
+    </ul>
+   
   </div>
 </template>
+
